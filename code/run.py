a9fe24fe1124d0d0c982f6d9cb7e75910d820ff4
dallascard <dcard@andrew.cmu.edu>
2017-06-28 10:33:53 -0700

    added models

--- code/run.py
+++ code/run.py
@@ -285,6 +285,19 @@ def run_analysis(subject, data_file, polls_file, first_year, last_year, group_by
         f.write(model.summary().as_text())
         f.write("\nRMSE=%0.4f" % rmse)
 
+
+    fitted, rmse, model = stats.ols(df=df, target='mood', columns=['prev_mood', 'surge_diff'], add_intercept=intercept)
+    with open(os.path.join(output_dir, 'surge_diff.txt'), 'a') as f:
+        f.write('\n\n' + subject + '\n=========\n')
+        f.write(model.summary().as_text())
+        f.write("\nRMSE=%0.4f" % rmse)
+
+    fitted, rmse, model = stats.ols(df=df, target='mood', columns=['prev_mood', 'surge_diff', 'stories'], add_intercept=intercept)
+    with open(os.path.join(output_dir, 'surge_diff__stories.txt'), 'a') as f:
+        f.write('\n\n' + subject + '\n=========\n')
+        f.write(model.summary().as_text())
+        f.write("\nRMSE=%0.4f" % rmse)
+
     fitted, rmse, model = stats.ols(df=df, target='mood', columns=['prev_mood', 'surge_diff_abs'], add_intercept=intercept)
     with open(os.path.join(output_dir, 'surge_diff_abs.txt'), 'a') as f:
         f.write('\n\n' + subject + '\n=========\n')
